2ebaa933d5be7ef4904077f7aac3d9f2da23cf42
Scotty Vernon <scott.vernon@gmail.com>
2019-01-20 15:49:27 +0000

    Cleanup PokemonSprite.jsx. Container does heavy lifting

--- src/components/PokemonSprite.jsx
+++ src/components/PokemonSprite.jsx
@@ -3,21 +3,6 @@ import React from "react";
 //TODO: import specific styles directly here (and move to CSS modules?)
 
 const PokemonSprite = (props) => {
-
-    /**
-     * direction (front/back)/colour (default/shiny)/gender (default/female)
-     *
-     * id.png
-     * back/id.png
-     * shiny/id.png
-     * back/shiny/id.png
-     * back/shiny/female/id.png
-     *
-     * female/id.png
-     */
-
-    //TODO: image url builder probably necessary
-
     return <img
         src={props.imgSrc}
         alt="pokemon"
